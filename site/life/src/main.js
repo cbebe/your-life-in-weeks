dc0cff92269630c59540a5296531cb819ad14073
Charles Ancheta <cancheta@ualberta.ca>
2020-07-07 21:18:00 -0600

    :sparkles: update week statement

--- site/life/src/main.js
+++ site/life/src/main.js
@@ -1,12 +1,52 @@
-function getGrid() {
-  return document.getElementById("grid");
+const grid = document.getElementById("grid");
+const dateInput = document.getElementById("birth");
+const weekStatement = document.getElementById("weeks");
+const dobForm = document.getElementById("date-of-birth");
+
+const WEEKS_IN_MS = 1000 * 3600 * 24 * 7;
+const NUMBER_OF_WEEKS = 52 * 80;
+const LIFESPAN = WEEKS_IN_MS * NUMBER_OF_WEEKS;
+
+let birth;
+let pastWeeksLived;
+let weeksLived;
+let today = new Date();
+
+function update() {
+  if (weeksLived != null) {
+    pastWeeksLived = weeksLived;
+  }
+
+  birth = new Date(dateInput.value);
+  console.log(birth);
+  weeksLived = Math.floor((today - birth) / WEEKS_IN_MS);
+  updateWeekStatement(weeksLived);
+  highlightCells(grid, 0, weeksLived);
 }
 
-// remember zero-indexing (September = 8)
-let birth = new Date(2000, 8, 12);
-let today = new Date(Date.now());
-const weekInMS = 1000 * 3600 * 24 * 7;
-let weeksLived = Math.floor((today - birth) / weekInMS);
+function updateWeekStatement(weeks) {
+  weekStatement.innerHTML = `So far, you have lived ${weeks} out of 4160 weeks.`;
+}
+
+function init() {
+  // create grid
+  for (let i = 0; i < NUMBER_OF_WEEKS; i++) appendCell(grid);
+
+  const oldestDate = new Date(today - LIFESPAN);
+
+  dateInput.min = getStringfromDate(oldestDate);
+  dateInput.max = getStringfromDate(today);
+
+  dobForm.addEventListener("submit", e => {
+    e.preventDefault();
+    update();
+  });
+
+  // remember zero-indexing (September = 8)
+  update();
+}
+
+init();
 
 function appendCell(grid) {
   const cell = document.createElement("div");
@@ -14,15 +54,27 @@ function appendCell(grid) {
   grid.appendChild(cell);
 }
 
-function highlightCells(grid) {
-  console.log(grid.children);
+function highlightCells(grid, head, tail) {
+  const pastWeeks = Array.from(grid.children).slice(head, tail);
+  pastWeeks.forEach(cell => {
+    cell.classList.add("past");
+  });
 }
 
-for (let i = 0; i < 52 * 80; i++) appendCell(getGrid());
+function unhighlightCells(grid, head, tail) {
+  const futureWeeks = Array.from(grid.children).slice(head, tail);
+  futureWeeks.forEach(cell => {
+    cell.classList.remove("past");
+  });
+}
 
-console.log(birth);
-console.log(today);
-console.log(weeksLived);
-console.log();
+function zeroPad(num, places) {
+  return String(num).padStart(places, "0");
+}
 
-highlightCells(getGrid());
+function getStringfromDate(date) {
+  const year = date.getFullYear();
+  const month = zeroPad(date.getMonth() + 1, 2);
+  const day = zeroPad(date.getDate(), 2);
+  return `${year}-${month}-${day}`;
+}
